5a4870f0cfe588a1743264485afb196a2731f4e4
indra <prince.poudel1998@gmail.com>
2019-12-05 10:34:13 +0545

    added assignment

--- script.js
+++ script.js
@@ -1,104 +1,78 @@
-// var cvs = document.getElementById("canvas");
-// /** @type {CanvasRenderingContext2D} */
-// var ctx = cvs.getContext("2d");
-// var x = cvs.width/2;
-// var y = cvs.height/3;
-// var MinballRadius = 0;
-// var MaxballRadius= 12
-// var BallsNo=15
+var animation1 = new Animation(600, 600);
+animation1.init();
 
-// function drawBall() {
-//     ctx.beginPath();
-//     ctx.arc(x, y, 12, 0, Math.PI*2, false);
-//     ctx.fillStyle = "#87ceeb";
-//     ctx.fill();
-//     ctx.closePath();
-// }
+function Animation(width, height) {
+  this.width = width;
+  this.height = height;
+  this.canvas;
+  this.context;
+  this.speed = 0.024;
+  this.totalRows = 11;
+  this.totalCols = 15;
+  this.phase = 0;
+  this.ballSize = 12;
+  this.colors = ["#ffae73", "#feaa77", "#fea57c", "#fea081", "#fe9b86", "#fa968b", "#f59190", "#f08c95", "#eb879a", "#e6829f", "#e17da4"];
+  var that = this;
 
-// drawBall();
+  this.init = function () {
+    this.createContainer();
+    //this.createBalls();
+    requestAnimationFrame(that.animation);
+    //that.render;
+  }
 
-function HelixAnimation() {
-  var cvs = document.getElementById("canvas");
-  /** @type {CanvasRenderingContext2D} */
-  var ctx = cvs.getContext("2d");
-  var frames=0;
+  this.createContainer = function () {
+    var body = document.getElementsByTagName('body')[0];
+    var container = document.createElement('div');
+    container.style.width = this.width + 'px';
+    container.style.height = this.height + 'px';
+    container.style.margin = '20px';
+    //container.style.background = 'url("./images/helix-animation.gif") center';
+    //container.style.backgroundSize = 'contain';
+    container.style.border = '3px solid black';
+    container.style.cssFloat = 'left';
+    container.classList.add('container');
+    body.appendChild(container);
 
-  //   this.drawBall= function()
-  //     {
-  //     this.ctx.beginPath();
-  //     this.ctx.arc(this.x, this.y, 12, 0, Math.PI*2, false);
-  //     this.ctx.fillStyle = "#87ceeb";
-  //     this.ctx.fill();
-  //     this.ctx.closePath();
-  //     }
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'canvas');
+    container.appendChild(canvas);
+    this.canvas = canvas;
+    that.context = that.canvas.getContext('2d');
+    that.canvas.width = this.width;
+    that.canvas.height = this.height;
+  }
 
-  this.ball = {
-    //    MinballRadius : 0,
-    MaxballRadius: 0,
-    color: "#87ceeb",
-    line: 100,
+  this.animation = function () {
+    that.context.fillStyle = "#043a4a";
+    that.context.rect(0, 0, that.canvas.width, that.canvas.height);
+    that.context.fill();
+    that.phase = requestAnimationFrame(that.animation) * that.speed;
+    for (var strand = 0; strand < 2; strand++) {
+      var strandPhase = that.phase + that.wavelength(strand, 0, 2, 0, 2 * Math.PI);
+      for (var col = 0; col < that.totalCols; col++) {
+        var colOffset = that.wavelength(col, 0, that.totalCols, 0, (Math.PI) + (Math.PI / 4));
+        var x = that.wavelength(col, 0, that.totalCols, 80, that.canvas.width - 60);
+        for (var row = 0; row < that.totalRows; row++) {
+          var y = (that.canvas.height / 3) + row * 0.032 * this.canvas.width + Math.sin(strandPhase + colOffset) * (that.canvas.width / 10);
+          var sizeOffset = (Math.cos(strandPhase - (row / that.totalRows) + colOffset) + 1) * (this.canvas.width / 1000);
+          var ballSizeNow = sizeOffset * that.ballSize;
 
-    draw: function(y,color1) {
-      this.x = cvs.width / 12;
-      this.y = y;
-      this.BallsNo = 15;
-
-       for (var j = 0; j < 5; j++) {
-         for (var i = 270; i > 90; i=i-5) {
-        
-          ctx.beginPath();
-          ctx.arc(
-            this.x,
-            ((Math.sin((frames-i)*(Math.PI/180)))*100)+this.y,
-           this.MaxballRadius+ Math.abs(Math.sin((frames%360))*(Math.PI/180))*15,     // this.MaxballRadius + Math.sin(i * 0.2) * 5,
-            0,
-            Math.PI * 2,
-            false
-          );
-          ctx.fillStyle = color1// "#FFB375";
-          ctx.fill();
-          ctx.closePath();
-          this.x = this.x +10*2 ;
-         
-          }
-          this.y = this.y + 10*5
-          this.x = cvs.width / 12;
+          that.context.beginPath();
+          that.context.clearRect(0, that.canvas.width, that.canvas.width, that.canvas.height);
+          that.context.arc(x, y, ballSizeNow, 0, 2 * Math.PI);
+          that.context.fillStyle = that.colors[row];
+          that.context.fill();
+          that.context.closePath();
         }
-          
-         
-        this.MaxballRadius= Math.sin((frames%180)*(Math.PI/180))*15;
-         console.log(frames);
-        }
-        
-       
-    //     this.x = cvs.width / 12;
-    //     this.y = this.y + 25;
-    //    // console.log(this.y);
-    //   }
-    //   this.y--;
-    // }
-  };
+      }
+    }
 
-  this.loop = function() {
-    this.update();
-    this.draw();
-    frames++;
-    requestAnimationFrame(this.loop.bind(this));
-  };
-  this.draw = function() {
-    ctx.clearRect(0, 0, cvs.width, cvs.height);
-    this.ball.draw(200,"#FFB375");
+  }
 
-    // bg.draw();
-    // pipes.draw();
-    // fg.draw();
-    // bird.draw();
-    // getReady.draw();
-    // gameOver.draw();
-    // score.draw();
-  };
+  this.wavelength = function (value, low1, high1, low2, high2) {
+    var length = low2 + (high2 - low2) * ((value - low1) / (high1 - low1));
+    return length;
+  }
 
-  // UPDATE
-  this.update = function() {};
 }
-new HelixAnimation().loop();
